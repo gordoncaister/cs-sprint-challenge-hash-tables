62f2301803fec9a9b8e773fad1520f171b4017c0
Gordon Caister <gordoncaister@gmail.com>
2020-07-10 10:57:56 -0500

    finished ex5 and mvp

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,5 +1,16 @@
 # Your code here
+"""
+actually gonna plan this one:
+I think the best thing to do here is figure out the index of the last '/' and then return the letters after that as the key and then the whole thing as the value. Then when you query files, you just lookup the key and return the value
 
+for file in files:
+    the cache key is files[lastindexof'/':] = file 
+
+for query in queries:
+    append cache[query] to result
+
+return result
+"""
 
 
 def finder(files, queries):
@@ -7,19 +18,56 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
+    for fil in files:
+        filname = fil[fil.rfind("/")+1:]
+        if filname not in cache:
+            cache[filname] = []
+        cache[filname].append(fil)
 
-    return result
+    for query in queries:
+        if query in cache:
+            for url in cache[query]:
+                result.append(url)
+
+    return(result)
 
 
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
+    # files = [
+    #     '/bin/foo',
+    #     '/bin/bar',
+    #     '/usr/bin/baz'
+    # ]
+    # queries = [
+    #     "foo",
+    #     "qux",
+    #     "baz"
+    # ]
+    # print(finder(files, queries))
+
+    files = []
+
+    for i in range(40):
+        files.append(f"/dir{i}/file{i}")
+
+    for i in range(40):
+        files.append(f"/dir{i}/dirb{i}/file{i}")
+
+    queries = []
+
+    for i in range(100):
+        queries.append(f"nofile{i}")
+
+    queries += [
+        "file12",
+        "file14",
+        "file39",
+        "file27"
     ]
-    queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
-    print(finder(files, queries))
+
+    result = finder(files, queries)
+    result.sort()
+
+    print(result)
